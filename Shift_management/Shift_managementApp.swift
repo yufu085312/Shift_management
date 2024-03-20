6ae136f450e61d6595d394268eebaf30812fa9de
福原優太 <fuyu0853@icloud.com>
2024-03-20 17:22:23 +0900

    Initial Commit

---
+++ Shift_management/Shift_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shift_managementApp.swift
+//  Shift_management
+//
+//  Created by k21101kk on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct Shift_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
